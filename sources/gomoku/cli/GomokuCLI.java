1abffb15574e58b48b87cc21bc3005d149f55b99
julien@macbook <julien.stechele@gmail.com>
2012-12-02 03:02:19 +0100

    Ajout du jeu en mode CLI (WIP)

---
+++ sources/gomoku/cli/GomokuCLI.java
@@ -0,0 +1,12 @@
+package gomoku.cli;
+
+import gomoku.jeu.Grille;
+import gomoku.regles.Gomoku;
+
+public class GomokuCLI {
+
+  public static void main (String[] args) {
+    Grille g = new Grille(new Gomoku());
+    System.out.println(g.toString());
+  }
+}
