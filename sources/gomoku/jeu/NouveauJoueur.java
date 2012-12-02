64cf271784ff891f22f3c130f6da1afe9ec02a9e
julien@macbook <julien.stechele@gmail.com>
2012-12-02 03:06:25 +0100

    Il y a un getter donc private

--- sources/gomoku/jeu/NouveauJoueur.java
+++ sources/gomoku/jeu/NouveauJoueur.java
@@ -2,14 +2,19 @@ package gomoku.jeu;
 
 public class NouveauJoueur implements Joueur {
 
-  // couleur attribué au joueur
-	protected int couleur;
+  /**
+   * couleur attribué au joueur
+   */
+	private int couleur;
 
   public NouveauJoueur (int c) {
     this.couleur = c;
   }
 
-  /** Couleur du joueur (retourne une des constantes NOIR ou BLANC) */
+  /*
+   * @override
+   * Couleur du joueur (retourne une des constantes NOIR ou BLANC)
+   */
   public int couleur() {
     return this.couleur;
   }	
