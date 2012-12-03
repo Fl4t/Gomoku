05d5006744861374941a86f72ccd59b960c85b32
julien@macbook <julien.stechele@gmail.com>
2012-12-04 00:16:14 +0100

    Re-écriture du main de la classe GomokuCLI

--- sources/gomoku/cli/GomokuCLI.java
+++ sources/gomoku/cli/GomokuCLI.java
@@ -2,11 +2,27 @@ package gomoku.cli;
 
 import gomoku.jeu.Grille;
 import gomoku.regles.Gomoku;
+import gomoku.jeu.NouveauJoueur;
+import gomoku.jeu.Joueur;
+import gomoku.jeu.Partie;
+import gomoku.jeu.Coordonnees;
 
 public class GomokuCLI {
 
   public static void main (String[] args) {
-    Grille g = new Grille(new Gomoku());
-    System.out.println(g);
+    Partie partie = new Partie(new NouveauJoueur(Joueur.NOIR),
+                          new NouveauJoueur(Joueur.BLANC),
+                          new Grille(new Gomoku()));
+    while (partie.coupAjouer()) {
+      Coordonnees c = partie.demanderCoor();
+      partie.placerPierreAuxCoor(c);
+      partie.rafraichirGrille();
+    }
+    if (partie.estGagne()) {
+      System.out.println("félicitation joueur " +
+          partie.joueurGagnant());
+    } else {
+      System.out.println("Partie nulle !");
+    }
   }
 }
