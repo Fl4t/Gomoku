038aebafcb670fcab6172254424d75a25c35ae77
julien@macbook <julien.stechele@gmail.com>
2012-12-04 00:09:29 +0100

    Un joueur a un nombre de coups (pierre) possibles

--- sources/gomoku/jeu/NouveauJoueur.java
+++ sources/gomoku/jeu/NouveauJoueur.java
@@ -6,16 +6,33 @@ public class NouveauJoueur implements Joueur {
    * couleur attribué au joueur
    */
 	private int couleur;
+  private int nbCoups;
 
   public NouveauJoueur (int c) {
     this.couleur = c;
+    this.nbCoups = 60;
   }
 
-  /*
+  /**
    * @override
    * Couleur du joueur (retourne une des constantes NOIR ou BLANC)
    */
   public int couleur() {
     return this.couleur;
-  }	
+  }
+
+  /**
+   * Retourne le nombre de coups restants
+   */
+  public int getNbCoups() {
+    return this.nbCoups;
+  }
+
+  /**
+   * Place une pierre et décremente le nombre de coups jouables
+   */
+  public void jouerUnCoup(Plateau p, Coordonnees c) {
+    p.placer(c, this.couleur());
+    this.nbCoups--;
+  }
 }
